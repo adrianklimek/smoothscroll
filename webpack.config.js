948f1b10ae38ef0eb0520557087edab794c96a6f
Adrian Klimek <adrianklimek@users.noreply.github.com>
2018-06-09 19:21:19 +0200

    Add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -3,25 +3,25 @@ const webpack = require('webpack')
 const UnminifiedWebpackPlugin = require('unminified-webpack-plugin')
 
 module.exports = {
-    entry: './src/smoothScroll.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        libraryTarget: 'umd',
-        libraryExport: 'default',
-        filename: 'smoothScroll.min.js'
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [{
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader'
-        }]
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            minimize: true,
-            sourceMap: true
-        }),
-        new UnminifiedWebpackPlugin()
-    ]
+  entry: './src/smoothScroll.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    filename: 'smoothScroll.min.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [{
+      exclude: /(node_modules)/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true
+    }),
+    new UnminifiedWebpackPlugin()
+  ]
 }
